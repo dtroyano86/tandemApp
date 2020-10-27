c97de02922ba4d4d8dbf16e56a1fabee5544b165
dtroyano86@gmail.com <dtroyano86@gmail.com>
2020-10-27 11:54:51 -0500

    Header SetUp

--- client/components/layout/header.jsx
+++ client/components/layout/header.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
-const Header = () => (
-  <div>Header</div>
+const Header = ({score}) => (
+  <Navbar>
+    <Navbar.Brand href="/">Home</Navbar.Brand>
+    <Navbar.Toggle aria-controls="header-navbar" />
+    <Navbar.Collapse id="header-navbar">
+      <Nav>
+        <Nav.Link link="/score">Scores</Nav.Link>
+      </Nav>
+      <Navbar.Text>
+        Score:
+        <span>
+          {score}
+        </span>
+      </Navbar.Text>
+    </Navbar.Collapse>
+  </Navbar>
 );
 
 export default Header;
