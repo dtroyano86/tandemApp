1cbebf9ec2452c8335137850431a3fc19adda07d
dtroyano86@gmail.com <dtroyano86@gmail.com>
2020-10-27 12:08:37 -0500

    Header PropTypes

--- client/components/layout/header.jsx
+++ client/components/layout/header.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 
-const Header = ({score}) => (
+const Header = ({ score }) => (
   <Navbar>
     <Navbar.Brand href="/">Home</Navbar.Brand>
     <Navbar.Toggle aria-controls="header-navbar" />
@@ -21,3 +22,7 @@ const Header = ({score}) => (
 );
 
 export default Header;
+
+Header.propTypes = {
+  score: PropTypes.number.isRequired,
+};
