ce83c6b283688f9148a683e265245e0e8cb34db5
dtroyano86@gmail.com <dtroyano86@gmail.com>
2020-10-27 13:32:26 -0500

    Question Page Setup

--- client/components/questionPage.jsx
+++ client/components/questionPage.jsx
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Jumbotron from 'react-bootstrap/Jumbotron';
 
+import trivia from '../Apprentice_TandemFor400_Data.json';
 import Question from './question';
 
-const QuestionPage = () => {
+const QuestionPage = ({ setScore }) => {
+  const [questionList, setQuestionList] = useState([]);
+
+  const beginQuiz = () => {
+    setQuestionList(trivia);
+  };
 
   return (
-    <div>
-      Question Page
-      <Question />
-    </div>
+    <Jumbotron>
+      {!questionList.length
+        ? (
+          <>
+            <h1>Get ready to answer some trivia!</h1>
+            <Button onClick={beginQuiz}>Bring it on!</Button>
+          </>
+        )
+        : (
+          <Question />
+        )}
+    </Jumbotron>
   );
 };
 
 export default QuestionPage;
+
+QuestionPage.propTypes = {
+  setScore: PropTypes.func.isRequired,
+};
