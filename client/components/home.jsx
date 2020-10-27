64cf00554765b2f0108dbb6a4388811b5a9e22f7
dtroyano86@gmail.com <dtroyano86@gmail.com>
2020-10-27 13:13:57 -0500

    React Bootstrap Router

--- client/components/home.jsx
+++ client/components/home.jsx
@@ -1,22 +1,18 @@
 import React, { useState } from 'react';
-import { Redirect } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Button from 'react-bootstrap/Button';
 
 const Home = ({setScore}) => {
-  const [redirect, setRedirect] = useState(false);
-
   const startTrivia = () => {
     setScore(0);
-    setRedirect(true);
   };
 
   return (
     <Jumbotron>
-      {redirect ? <Redirect push to="/trivia" /> : null}
       <h1>Trivia Questions</h1>
       <p>Get ready to learn some trivia!</p>
-      <Button onClick={startTrivia}>Start Trivia!</Button>
+      <Link to="/trivia">Start Trivia!</Link>
     </Jumbotron>
   );
 };
