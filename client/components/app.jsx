662b9ee9cee2858cf5fc2fa7a3401d8e351537b6
dtroyano86@gmail.com <dtroyano86@gmail.com>
2020-10-27 10:58:19 -0500

    React Bootstrap Setup

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+
+import Footer from './layout/footer';
+import Header from './layout/header';
+import Home from './home';
 
 const app = () => (
-  <h1>Hello from React</h1>
+  <BrowserRouter>
+    <Container>
+      <Row>
+        <Header />
+      </Row>
+      <Row>
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Row>
+      <Row>
+        <Footer />
+      </Row>
+    </Container>
+  </BrowserRouter>
 );
 
 export default app;
