49707f05cbca415349c55f73b8dbeb1372d2d171
dtroyano86@gmail.com <dtroyano86@gmail.com>
2020-10-27 12:13:24 -0500

    Basic Question Setup

---
+++ client/components/questionPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Question from './question';
+
+const QuestionPage = () => {
+
+  return (
+    <div>
+      Question Page
+      <Question />
+    </div>
+  );
+};
+
+export default QuestionPage;
