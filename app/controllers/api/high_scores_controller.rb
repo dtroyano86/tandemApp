47a24d8c96a8cc7f3254500675a6629d7ae48eea
dtroyano86@gmail.com <dtroyano86@gmail.com>
2020-10-27 15:44:28 -0500

    High Score Model Setup

---
+++ app/controllers/api/high_scores_controller.rb
@@ -0,0 +1,32 @@
+class Api::HighScoresController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def index
+    high_scores = HighScore.all
+    render json: high_scores
+  end
+
+  def create
+    high_score = HighScore.new(high_score_params)
+    if high_score.save!
+      render json: high_score
+    else
+      render json: high_score.errors
+    end
+  end
+
+  def destroy
+    high_score = HighScore.find(params[:id])
+    if high_score.destroy!
+      render json: high_score
+    else
+      render json: high_score.errors
+    end
+  end
+
+  private
+
+  def high_score_params
+    params.require(:high_score).permit(:name, :score)
+  end
+end
